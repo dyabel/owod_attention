5b3e442237d7a0803b4a5d47f3412ef18f6e7c68
dyabel <abelazady@foxmail.com>
2021-05-28 16:51:37 +0800

    update

--- run_batch8.sh
+++ run_batch8.sh
@@ -11,50 +11,50 @@
 # (if you find something wrong in this, please raise an issue on GitHub)
 
 # Task 1
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52125' --config-file ./configs/OWOD/t1/t1_train_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t1_b8"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52125' --config-file ./configs/OWOD/t1/t1_train_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t1_b8"
 
 # No need to finetune in Task 1, as there is no incremental component.
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t1/t1_val_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t1_final_b8" MODEL.WEIGHTS "output/t1_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t1/t1_val_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t1_final_b8" MODEL.WEIGHTS "output/t1_b8/model_final.pth"
 #
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --eval-only --config-file ./configs/OWOD/t1/t1_test_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t1_final_b8" MODEL.WEIGHTS "output/t1_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD/t1/t1_test_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t1_final_b8" MODEL.WEIGHTS "output/t1_b8/model_final.pth"
 
 
 # Task 2
 cp -r output/t1_b8 output/t2_b8
 #
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52127'  --resume --config-file ./configs/OWOD/t2/t2_train_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2_b8" MODEL.WEIGHTS "output/t2_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52127'  --resume --config-file ./configs/OWOD/t2/t2_train_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2_b8" MODEL.WEIGHTS "output/t2_b8/model_final.pth"
 
 cp -r output/t2_b8 output/t2_ft_b8
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52126'  --resume --config-file ./configs/OWOD/t2/t2_ft_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2_ft_b8" MODEL.WEIGHTS "output/t2_ft_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52126'  --resume --config-file ./configs/OWOD/t2/t2_ft_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2_ft_b8" MODEL.WEIGHTS "output/t2_ft_b8/model_final.pth"
 #
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t2/t2_val_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t2_final_b8" MODEL.WEIGHTS "output/t2_ft_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t2/t2_val_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t2_final_b8" MODEL.WEIGHTS "output/t2_ft_b8/model_final.pth"
 #
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --eval-only --config-file ./configs/OWOD/t2/t2_test_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2_final_b8" MODEL.WEIGHTS "output/t2_ft_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD/t2/t2_test_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2_final_b8" MODEL.WEIGHTS "output/t2_ft_b8/model_final.pth"
 
 
 # Task 3
 cp -r output/t2_ft_b8 output/t3_b8
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t3/t3_train_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3_b8" MODEL.WEIGHTS "output/t3_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t3/t3_train_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3_b8" MODEL.WEIGHTS "output/t3_b8/model_final.pth"
 
 cp -r output/t3_b8 output/t3_ft_b8
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t3/t3_ft_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3_ft_b8" MODEL.WEIGHTS "output/t3_ft_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t3/t3_ft_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3_ft_b8" MODEL.WEIGHTS "output/t3_ft_b8/model_final.pth"
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t3/t3_val_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t3_final_b8" MODEL.WEIGHTS "output/t3_ft_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t3/t3_val_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t3_final_b8" MODEL.WEIGHTS "output/t3_ft_b8/model_final.pth"
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --eval-only --config-file ./configs/OWOD/t3/t3_test_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3_final_b8" MODEL.WEIGHTS "output/t3_ft_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD/t3/t3_test_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3_final_b8" MODEL.WEIGHTS "output/t3_ft_b8/model_final.pth"
 
 
 # Task 4
 cp -r output/t3_ft_b8 output/t4_b8
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t4/t4_train_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t4_b8" MODEL.WEIGHTS "output/t4_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t4/t4_train_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t4_b8" MODEL.WEIGHTS "output/t4_b8/model_final.pth"
 
 cp -r output/t4 output/t4_ft_b8
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t4/t4_ft_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t4_ft_b8" MODEL.WEIGHTS "output/t4_ft_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t4/t4_ft_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t4_ft_b8" MODEL.WEIGHTS "output/t4_ft_b8/model_final.pth"
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/train_net.py --num-gpus 4 --eval-only --config-file ./configs/OWOD/t4/t4_test_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t4_final_b8" MODEL.WEIGHTS "output/t4_ft_b8/model_final.pth"
+python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD/t4/t4_test_b8.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t4_final_b8" MODEL.WEIGHTS "output/t4_ft_b8/model_final.pth"
